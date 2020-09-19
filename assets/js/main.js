210ef624a6a8503678f98a81c5277c879aa1403e
Cycvez <57923828+Cycvez@users.noreply.github.com>
2020-09-18 23:58:52 -0500

    social media update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Remember - v2.1.0
-* Template URL: https://bootstrapmade.com/remember-free-multipurpose-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 !(function($) {
   "use strict";
 
